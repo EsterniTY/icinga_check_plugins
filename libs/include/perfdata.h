1aa7c4b403c8c24b7d11f0d59c9ae4643eb53a62
Sergey Gribinichenko <developer.kg@gmail.com>
2019-02-06 12:37:46 +0300

    Add perfdata_* functions

---
+++ libs/include/perfdata.h
@@ -0,0 +1,56 @@
+#ifndef PERFDATA_H
+#define PERFDATA_H
+
+#include <sys/types.h>
+
+#define UOM_NORMAL  0
+#define UOM_PERCENT 1
+#define UOM_SECONDS 2
+#define UOM_BYTES   3
+#define UOM_COUNTER 4
+
+typedef u_int8_t uom_t;
+typedef u_int64_t bytes_t;
+typedef double percent_t;
+
+struct perfdata {
+    uom_t uom;
+
+    char *name;
+    size_t name_len;
+
+    struct {
+        bytes_t value;
+        bytes_t warn;
+        bytes_t crit;
+        bytes_t min;
+        bytes_t max;
+    } bytes;
+
+    struct {
+        percent_t value;
+        percent_t warn;
+        percent_t crit;
+        percent_t min;
+        percent_t max;
+    } percent;
+
+    struct perfdata *next;
+};
+
+void perfdata_add_bytes(struct perfdata **root,
+                        char *name, size_t name_len,
+                        bytes_t value,
+                        bytes_t warn, bytes_t crit,
+                        bytes_t min, bytes_t max);
+
+void perfdata_add_percent(struct perfdata **root,
+                          char *name, size_t name_len,
+                          percent_t value,
+                          percent_t warn, percent_t crit,
+                          percent_t min, percent_t max);
+
+void perfdata_print(struct perfdata *pf);
+void perfdata_free(struct perfdata *pf);
+
+#endif /* PERFDATA_H */
---
+++ libs/src/perfdata.c
@@ -0,0 +1,109 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "perfdata.h"
+
+void perfdata_add_bytes(struct perfdata **root,
+                        char *name, size_t name_len,
+                        bytes_t value,
+                        bytes_t warn, bytes_t crit,
+                        bytes_t min, bytes_t max)
+{
+    struct perfdata *_root = *root;
+    struct perfdata *new = malloc(sizeof (struct perfdata));
+
+    new->uom = UOM_BYTES;
+    new->name_len = name_len;
+    new->name = malloc(name_len + 1);
+    memcpy(new->name, name, name_len);
+    new->name[name_len] = '\0';
+
+    new->bytes.value = value;
+    new->bytes.warn = warn;
+    new->bytes.crit = crit;
+    new->bytes.min = min;
+    new->bytes.max = max;
+
+    new->next = NULL;
+
+    if (_root != NULL)
+        _root->next = new;
+
+    *root = new;
+}
+
+void perfdata_add_percent(struct perfdata **root,
+                          char *name, size_t name_len,
+                          percent_t value,
+                          percent_t warn, percent_t crit,
+                          percent_t min, percent_t max)
+{
+    struct perfdata *_root = *root;
+    struct perfdata *new = malloc(sizeof (struct perfdata));
+
+    new->uom = UOM_PERCENT;
+    new->name_len = name_len;
+    new->name = malloc(name_len + 1);
+    memcpy(new->name, name, name_len);
+    new->name[name_len] = '\0';
+
+    new->percent.value = value;
+    new->percent.warn = warn;
+    new->percent.crit = crit;
+    new->percent.min = min;
+    new->percent.max = max;
+
+    new->next = NULL;
+
+    if (_root != NULL)
+        _root->next = new;
+
+    *root = new;
+}
+
+void perfdata_print(struct perfdata *pf)
+{
+    struct perfdata *curr = pf;
+
+    while (curr != NULL) {
+        switch (curr->uom) {
+        case UOM_BYTES:
+            printf("'%s'=%luB;%lu;%lu;%lu;%lu", curr->name,
+                   curr->bytes.value,
+                   curr->bytes.warn, curr->bytes.crit,
+                   curr->bytes.min, curr->bytes.max);
+            break;
+        case UOM_PERCENT:
+            printf("'%s'=%.2f%%;%.2f;%.2f;%.2f;%.2f", curr->name,
+                   curr->percent.value,
+                   curr->percent.warn, curr->percent.crit,
+                   curr->percent.min, curr->percent.max);
+            break;
+        }
+
+        if (curr->next)
+#ifdef DEBUG
+            printf("\n");
+#else
+            printf(" ");
+#endif
+        else
+            puts("");
+
+        curr = curr->next;
+    }
+}
+
+void perfdata_free(struct perfdata *pf)
+{
+    struct perfdata *next = pf->next;
+
+    if (pf->name)
+        free(pf->name);
+
+    free(pf);
+
+    if (next)
+        perfdata_free(next);
+}
