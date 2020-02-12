ef239583db340ebd4a6844ad258ffb80ab165d78
Esternity <developer.kg@gmail.com>
2020-02-12 12:56:24 +0300

    Detect host vendor (MikroTik only for now)

--- check_iftraffic/check_iftraffic.h
+++ check_iftraffic/check_iftraffic.h
@@ -48,6 +48,7 @@ extern struct opt_s {
 extern struct host_settings_s {
     u_int8_t has_ifSpeed64;
     u_int    uptime;
+    u_int8_t vendor;
 } host_settings;
 
 extern struct if_status_t if_status[];
